c5c8774281648e66a1902152c659079914e239ce
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2021-02-05 11:55:07 +0100

    Initialize the Diagram and Classes with their starting Properties.

---
+++ H1School_CarWAshSystem-SLN/H1School_CarWAshSystem/Vehicle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace H1School_CarWAshSystem
+{
+    public class Vehicle
+    {
+    }
+}
---
+++ H1School_CarWAshSystem-SLN/H1School_CarWAshSystem/WashingType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace H1School_CarWAshSystem
+{
+    public class WashingType
+    {
+        public List<Processes> Process { get; set; }
+
+        public WashingType(Processes process)
+        {
+            Process = new List<Processes>();
+            Process = process;
+        }
+    }
+}
